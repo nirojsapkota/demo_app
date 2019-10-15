0a74baf8608a5d7eb283d75cdfa115705b633800
Niroj Sapkota <niroj@onebigswitch.com>
2019-10-15 23:17:07 +1100

    code cleanup

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -29,53 +29,7 @@ class ImportsController < ApplicationController
           f.close
         end
 
-ImportCsvJob.perform_later(filename)
-
-#######################
-
-# csv_file = File.read(Rails.root.join('public', 'uploads', filename))
-#     logger.info("importing #{filename} file")
-#     error_import = []
-#     CSV.parse(csv_file, :headers => true).each do |row|
-#       formatted_hash = {}
-#       # lowercase the keys
-#       # split name to first_name and last_name
-#       # formatted_hash = row.to_h.map{|k, v| formatted_hash.merge!({k.downcase => v})}
-
-#       formatted_hash = Hash[row.to_h.map { |k, v| [k.downcase, v] }]
-#       name = formatted_hash["name"]
-#       if name
-#         formatted_hash.delete("name")
-#         name_split = name.split(" ")
-#         formatted_hash.merge!(Hash["first_name", name_split[0], "last_name", name_split.size > 1 ? name_split.last : ''])
-#       end
-
-#       # create person object
-#       p = Person.new(formatted_hash.reject{|k, v| %w(affiliations location).include?(k.to_s)})
-#       # process each for location and affiliation keys
-#       unless formatted_hash['affiliations'].nil?
-#         formatted_hash['affiliations'].split(",").each do |aff|
-#           p.affiliations << Affiliation.create({name: aff})
-#         end
-#       end
-
-#       unless formatted_hash['location'].nil?
-#         formatted_hash['location'].split(",").each do |loc|
-#           p.locations<< Location.create({name: loc})
-#         end
-#       end
-#       byebug
-
-#       unless p.valid?
-#         puts p.errors.messages
-#       end
-
-#       p.valid? ? p.save : error_import << formatted_hash
-#     end
-
-#######################
-
-
+        ImportCsvJob.perform_later(filename)
       rescue StandardError => e
         error = e.message
       end
